096bbead59e7d239f0ecebe35aea7e50545bc9c1
Ubuntu <ubuntu@ip-172-20-73-27.eu-west-1.compute.internal>
2014-06-23 18:11:08 +0000

    add 5

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World no 4!" );
+        System.out.println( "Hello World no 5!" );
     }
 }
