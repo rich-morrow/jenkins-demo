375f4176560dcdeb2bd9aadabb9e690441be2c6b
Ubuntu <ubuntu@ip-172-20-73-27.eu-west-1.compute.internal>
2014-06-26 18:19:29 +0000

    Germany Won USA 1:0 ha

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello C2S Folks!, Coffee Break ?" );
+        System.out.println( "Hello C2S Folks!, Coffee Break 2?" );
     }
 }
