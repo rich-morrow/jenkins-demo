dfa8b5169c646fd32e168a7b6557be72e95c4833
Ubuntu <ubuntu@ip-172-31-41-149.eu-west-1.compute.internal>
2015-02-19 17:18:10 +0000

    changes

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -9,7 +9,5 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Sup dawg? no 40!" );
-        System.out.println( "Sup dawg? no 40!" );
-        System.out.println( "Sup dawg? no 40!" );
     }
 }
